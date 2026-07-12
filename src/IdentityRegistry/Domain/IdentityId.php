d05cf791d18844d580bdbbbd30f96b939c543926
zumradeals <zumradeals@gmail.com>
2026-07-12 08:41:35 +0000

    feat(identity): add immutable identity identifier value object

---
+++ src/IdentityRegistry/Domain/IdentityId.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gamad\Core\IdentityRegistry\Domain;
+
+use InvalidArgumentException;
+
+final readonly class IdentityId
+{
+    private const PATTERN = '/^GAM-[A-Z]{3}-[0-9]{6,}$/';
+
+    public function __construct(public string $value)
+    {
+        if (preg_match(self::PATTERN, $value) !== 1) {
+            throw new InvalidArgumentException('Invalid GAMAD identity identifier.');
+        }
+    }
+
+    public function equals(self $other): bool
+    {
+        return $this->value === $other->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
