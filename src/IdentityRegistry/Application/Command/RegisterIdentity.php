537272a9c3f5b64c2298c81fccc910b8d2c69ed3
zumradeals <zumradeals@gmail.com>
2026-07-13 12:48:39 +0000

    refactor(identity): make identifier generation server authoritative

--- src/IdentityRegistry/Application/Command/RegisterIdentity.php
+++ src/IdentityRegistry/Application/Command/RegisterIdentity.php
@@ -5,15 +5,13 @@ declare(strict_types=1);
 namespace Gamad\Core\IdentityRegistry\Application\Command;
 
 use DateTimeImmutable;
-use Gamad\Core\IdentityRegistry\Domain\IdentityId;
 use Gamad\Core\IdentityRegistry\Domain\IdentityType;
 
 final readonly class RegisterIdentity
 {
     public function __construct(
-        public IdentityId $identityId,
         public IdentityType $identityType,
+        public string $actorId,
         public DateTimeImmutable $registeredAt,
-    ) {
-    }
+    ) {}
 }
